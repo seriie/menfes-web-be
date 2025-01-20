9b07caa4a98e9bf1dd58dd77d2ab2ae41441641d
serrie <mohammadzidane058@gmail.com>
2025-01-20 22:46:12 +0700

    setting SEO

--- index.js
+++ index.js
@@ -5,17 +5,51 @@ const bodyParser = require("body-parser");
 const authRoutes = require('./routes/auth');
 const menfesRoutes = require('./routes/menfes');
 const profileRoutes = require('./routes/profile');
+const compression = require('compression');
+const { SitemapStream, streamToPromise } = require('sitemap');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use(compression());
 
 app.get("/", (req, res) => res.send("Server is running!"));
 app.use("/auth", authRoutes);
 app.use("/menfes", menfesRoutes);
 app.use('/profile', profileRoutes);
 
-const PORT = 9000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
-// module.exports = app;
+app.get('/robots.txt', (req, res) => {
+    res.sendFile(path.join(__dirname, 'robots.txt'));
+});
+
+app.get('/sitemap.xml', async (req, res) => {
+    try {
+        const frontendBaseUrl = process.env.FRONTEND_BASE_URL;
+
+        const sitemap = new SitemapStream({ hostname: frontendBaseUrl });
+        
+        // Route frontend
+        const data = [
+          { url: '/', changefreq: 'daily', priority: 1.0 },
+          { url: '/login', changefreq: 'monthly', priority: 0.8 },
+          { url: '/register', changefreq: 'monthly', priority: 0.8 },
+          { url: '/create-menfes', changefreq: 'weekly', priority: 0.9 },
+          { url: '/inbox', changefreq: 'daily', priority: 0.7 },
+          { url: '/profile', changefreq: 'weekly', priority: 0.6 },
+        ];
+
+        // Convert data to XML sitemap
+        const xml = await streamToPromise(sitemap.end(data));
+
+        // Send response
+        res.header('Content-Type', 'application/xml');
+        res.send(xml.toString());
+    } catch (error) {
+      res.status(500).send('Error generating sitemap');
+    }
+});  
+
+// const PORT = 9000;
+// app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+module.exports = app;
