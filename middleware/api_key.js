cf4f3aeb8df5a5090f7d2930799a7f4850b28a9d
serrie <mohammadzidane058@gmail.com>
2025-02-05 18:17:59 +0700

    remove debugging

--- middleware/api_key.js
+++ middleware/api_key.js
@@ -4,8 +4,6 @@ require('dotenv').config();
 function verifyApiKey(req, res, next) {
     const validApiKeys = process.env.VALID_API_KEY ? process.env.VALID_API_KEY.split(',') : [];
     const apiKey = req.query.KEY;
-    console.log("Query Params:", req.query); // Cek query params yang masuk
-    console.log("Received API Key:", req.query.key); // Cek apakah key terbaca
     if (!apiKey) {
         return res.status(401).json({ error: 'API key is missing!' });
     }
